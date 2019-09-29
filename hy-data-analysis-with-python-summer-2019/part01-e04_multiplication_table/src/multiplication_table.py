67aca3efeb0940b3e026e1c34686a0ea5f378a7a
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-09-28 20:10:39 -0400

    a few more problems

--- hy-data-analysis-with-python-summer-2019/part01-e04_multiplication_table/src/multiplication_table.py
+++ hy-data-analysis-with-python-summer-2019/part01-e04_multiplication_table/src/multiplication_table.py
@@ -3,6 +3,11 @@
 
 def main():
     pass
+    for i in range(1,11):
+        for j in range(1,11):
+            print(" " + str(i*j), end="")
+        print() #new line
+
 
 if __name__ == "__main__":
     main()
--- hy-data-analysis-with-python-summer-2019/part01-e05_two_dice/src/two_dice.py
+++ hy-data-analysis-with-python-summer-2019/part01-e05_two_dice/src/two_dice.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    for i in range(1,7):
+        for h in range(1,7):
+            if i + h == 5:
+                print("(" + str(i) + "," + str(h) + ")")
+
 
 if __name__ == "__main__":
     main()
