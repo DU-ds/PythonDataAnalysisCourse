2c1a092189be72c8a9445654d455efb43330bcc7
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-09-28 20:42:37 -0400

    completed a few more problems

--- hy-data-analysis-with-python-summer-2019/part01-e06_triple_square/src/triple_square.py
+++ hy-data-analysis-with-python-summer-2019/part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
 
+def triple(a):
+    """returns a*3"""
+    return a*3
+def square(a):
+    """returns a squared"""
+    return a*a
 def main():
-    pass
+    i = 1
+    for i in range(1,11):
+        trip = triple(i)
+        sqr = square(i)
+        if sqr <= trip:
+            print("triple(%d)==%d square(%d)==%d" % (i, trip, i, sqr))
+        else:
+            break
+
 
 if __name__ == "__main__":
     main()
