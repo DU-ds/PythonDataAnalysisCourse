31dfa43e813289780ccfa8c590d24ecf2c226249
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-09-30 17:55:03 -0400

    two questions from part 2

--- hy-data-analysis-with-python-summer-2019/part02-e01_integers_in_brackets/src/integers_in_brackets.py
+++ hy-data-analysis-with-python-summer-2019/part02-e01_integers_in_brackets/src/integers_in_brackets.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-
+import re
 
 def integers_in_brackets(s):
-    return []
+    """finds all integers enclosed in brackets"""
+    lst = re.findall(r"\[\s*([-+]{0,1}\d+)\s*\]", s)
+    return list(map(int, lst))
 
 def main():
-    pass
+    print(integers_in_brackets("  afd [asd] [12 ] [a34]  [ -43 ]tt [+12]xxx"))
 
 if __name__ == "__main__":
     main()
--- hy-data-analysis-with-python-summer-2019/part02-e02_file_listing/src/file_listing.py
+++ hy-data-analysis-with-python-summer-2019/part02-e02_file_listing/src/file_listing.py
@@ -4,7 +4,29 @@ import re
 
 
 def file_listing(filename="src/listing.txt"):
-    return []
+    """
+    Args:
+        filename: string
+            file with file information (think ls -l)
+    Returns:
+        lst: list
+            contains tuples (size, month, day, hour, minute, filename)
+    """
+    lst = []
+    with open(filename, "r") as f:
+        for li in f:
+            lst.append(file_regex(li))
+    return lst
+        
+def file_regex(line):
+    """takes a line, parses it, returns the tuple needed by file_listing"""
+        # (size, month, day, hour, minute, filename)
+    regex = r"[-rwxd]{9}\s\d+\s\w*\s[a-zA-Z0-9\-]*\s*(\d*)\s(\w{3})\s+(\d{1,2})\s(\d{2})\:(\d{2})\s([^\n]+)"#\n"
+    lst = re.findall(regex, line)
+    t = lst[0]
+    lst = [int(t[0]), t[1], int(t[2]), int(t[3]), int(t[4]), t[5]]
+    t = tuple(lst) 
+    return t
 
 def main():
     pass
