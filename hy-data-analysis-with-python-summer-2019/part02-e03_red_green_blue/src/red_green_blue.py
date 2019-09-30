e1941c31ccdf78212cafd404b2bd1775d354dcaa
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-09-30 19:03:09 -0400

    wrote code for rgb. Need to test and submit still

--- hy-data-analysis-with-python-summer-2019/part02-e03_red_green_blue/src/red_green_blue.py
+++ hy-data-analysis-with-python-summer-2019/part02-e03_red_green_blue/src/red_green_blue.py
@@ -3,11 +3,25 @@
 import re
 
 def red_green_blue(filename="src/rgb.txt"):
-    return []
+    lst = []
+    with open(filename, "r") as f:
+        l1 = f.readline()
+        for li in f:
+            lst.append(string_cleaner(li))
+    return lst
+
+def string_cleaner(s):
+    """
+    cleans the individual strings to have format "num\tnum\tnum\tname"
+    """
+    regex = "(\d*)\s*(\d*)\s*(\d*)\s*(\w*)\n"
+    lst = re.findall(regex, s)
+    lst = list(lst[0]) #list(tuple)
+    return "\t".join(lst)
 
 
 def main():
-    pass
+    print(red_green_blue())
 
 if __name__ == "__main__":
     main()
