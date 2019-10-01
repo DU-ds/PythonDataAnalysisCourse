a524a2572e61a24878834baa4bd67449997bbdf8
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-10-01 03:24:41 -0400

    worked on word_freq*

--- hy-data-analysis-with-python-summer-2019/part02-e04_word_frequencies/src/word_frequencies.py
+++ hy-data-analysis-with-python-summer-2019/part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
 def word_frequencies(filename):
-    return {}
+    dikt = {}
+with open(filename, "r") as f:
+    lst = f.readlines()
+    lst2= []
+    lst = list(map(lambda x: x.split(), lst))
+for x in lst:
+    for e in x:
+        lst2.append(e.strip("""!"#$%&'()*,-./:;?@[]_"""))
+    # lst = list(map((lambda x: e.strip("""!"#$%&'()*,-./:;?@[]_""") for e in x), lst))
+    for s in lst2:
+        if s in dikt:
+            dikt[s] += 1
+        else:
+            dikt[s] = 1
 
 def main():
-    pass
+    print(word_frequencies(filename='R:/Git/PythonDataAnalysisCourse/hy-data-analysis-with-python-summer-2019/part02-e04_word_frequencies/src/alice.txt'))
 
 if __name__ == "__main__":
     main()
+
