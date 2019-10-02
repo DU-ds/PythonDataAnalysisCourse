6bcabd7de5c6375b2c6b3da3427b6690fe39d7b8
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-10-02 19:01:34 -0400

    working on rational. Finished prepend

--- hy-data-analysis-with-python-summer-2019/part02-e08_prepend/src/prepend.py
+++ hy-data-analysis-with-python-summer-2019/part02-e08_prepend/src/prepend.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 class Prepend(object):
-    # Add the methods of the class here
+    
+    def __init__(self, s):
+        self.start = s
+    
+    def write(self, s):
+        print(self.start + s)
 
 def main():
-    pass
+    prep = Prepend("Don't forget to ")
+    prep.write("be awesome")
 
 if __name__ == "__main__":
     main()
--- hy-data-analysis-with-python-summer-2019/part02-e09_rational/src/rational.py
+++ hy-data-analysis-with-python-summer-2019/part02-e09_rational/src/rational.py
@@ -1,8 +1,57 @@
 #!/usr/bin/env python3
+import math
 
 class Rational(object):
-    pass
+    
+    def __init__(self, top, bottom):
+        self.numerator = top
+        self.denomonator = bottom
+    
+    def __add__(self, ):
+    
+    def __sub__(self, ):
+        
+    def __truediv__(self, ):
+        
+    def __lt__(self, ):
+        
+    def __gt__(self, ):
+        
+    def __eq__(self, ):
+        
+    def reduce_fraction(top, bottom): #NOTE UNTESTED
+        """function returns a tuple(top, bottom) in reduced form
+        
+        #recursive calls
+        #case 1: bottom mod top == 0, e.g. 4/8 --> 8 mod 4 == 0
+            #since 4/4 = 1 and 8/4 = 2
+        #case 2: 
+        #case 3: top mod bottom == 0
+        
+        #base case: a mod top == 0 and a mod bottom == 0
+            #divide both by a and call both
+        
+        
+    realized I needed primes to do it efficiently
+    https://stackoverflow.com/questions/15706911/generator-in-python-generating-prime-numbers
+    going to find base case, reduce by dividing top and bottom
+    then recursing( calling the function again and the reduced top and bottom)
+    stopping when reaching max(sqrt(top), sqrt(bottom)) 
+        """
+        for i in range(max(top,bottom)): #going to break out of loop when small enough
+            if i > max(math.sqrt(top), math.sqrt(bottom)): 
+            # if n is a perfect square, sqrt(n) is the largest prime factor 
+                # that would not have another factor found by searching
+            # otherwise sqrt(n) is greater than any prime factor
+            # e.g. 16. factors, 2,4,8. 2 *4 = 16, 4 * 4 = 16. You would find 2 and divide 
+                return (top, bottom) #coprime
+            if i % top == 0 && i % bottom == 0:
+                top /= i
+                bottom /= i
+                return self(top, bottom)
+                
 
+        
 def main():
     r1=Rational(1,4)
     r2=Rational(2,3)
