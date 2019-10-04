0d875b2868bad58f7b602dc5a7b14aa4d621da5e
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-10-04 16:09:28 -0400

    got up to frequency sorting. Stuck there.

--- hy-data-analysis-with-python-summer-2019/part02-e15_vector_angles/src/vector_angles.py
+++ hy-data-analysis-with-python-summer-2019/part02-e15_vector_angles/src/vector_angles.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import math
 import numpy as np
 import scipy.linalg
 
@@ -9,13 +10,19 @@ def vector_angles(X, Y):
     y_dot_y = np.sum(Y * Y, axis = 1)
     cos_xy = x_dot_y/(np.sqrt(y_dot_y) * np.sqrt(x_dot_x))
     cos_xy = np.clip(cos_xy, a_min = -1, a_max = 1)
-    return np.arccos(cos_xy)
+    x_y = np.arccos(cos_xy)
+    x_y = (x_y/math.pi) * 180
+    return x_y
 
 # https://docs.scipy.org/doc/numpy/reference/generated/numpy.einsum.html#numpy.einsum
 
 def main():
-    X = numpy.arange(12).reshape(3,4)
-    Y =  2 * numpy.random.randint(0, 12, size = (3, 4))
+    X = np.arange(12).reshape(3,4)
+    Y =  2 * np.random.randint(0, 12, size = (3, 4))
+    angle = vector_angles(X, Y)
+    print(angle)
+    X = np.array([[0, 0, 1],[-1, 1, 0]])
+    Y = np.array([[0, 1, 0],[1, 1, 0]])
     angle = vector_angles(X, Y)
     print(angle)
 
