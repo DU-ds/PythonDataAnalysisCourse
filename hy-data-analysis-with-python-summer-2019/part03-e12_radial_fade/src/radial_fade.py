2072dd0e5d90806ae6bc7b0918933df866bb1fb2
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-10-06 05:11:41 -0400

    still working on radial_fade

--- hy-data-analysis-with-python-summer-2019/part03-e12_radial_fade/src/radial_fade.py
+++ hy-data-analysis-with-python-summer-2019/part03-e12_radial_fade/src/radial_fade.py
@@ -4,10 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def center(a):
-    return (0,0)   # note the order: (center_y, center_x)
+    nrows, ncols = a.shape[:2]
+    return (nrows/2,ncols/2)   # note the order: (center_y, center_x)
 
 def radial_distance(a):
-    return np.array([[]])
+    nrows, ncols = a.shape[:2]
+    center_x, center_y = center(a)
+    G = ( ((x - center_x)**2 + (y - center_y)**2)**(1/2) 
+         for x in range(nrows)
+         for y in range(ncols))
+    return np.array([(x, y) for r in ]) # create teh 2d array. 
 
 def scale(a, tmin=0.0, tmax=1.0):
     """Returns a copy of array 'a' with its values scaled to be in the range
