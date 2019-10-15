6e5184ee0eaa4b0c32e434fae2a990ad3cf3f8d2
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-10-15 13:43:53 -0400

    submitted first two linear regression problems

--- hy-data-analysis-with-python-summer-2019/part05-e10_linear_regression/src/linear_regression.py
+++ hy-data-analysis-with-python-summer-2019/part05-e10_linear_regression/src/linear_regression.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
-
+import math
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 def fit_line(x, y):
-    return 0, 0
-    
+    fm = LinearRegression(fit_intercept = True)
+    fm.fit(x[:, None], y)
+    intercept = fm.intercept_
+    slope = fm.coef_
+    return slope[0], intercept
+
 def main():
+    x1 = np.linspace(1, 100, 100)
+    # x2 = np.array([0,1,2,3,4,5,6,7,8,9])
+    y1 = 4 * x1 * np.random.randn(100) + 12 + 1 * np.random.randn(100)
+    # y2 = math.sqrt(2) + x2 * np.random.randn(10) 
+    slope1, intercept1 = fit_line(x1,y1)
+    # slope2, intercept2 = fit_line(x2,y2)
+    print("Slope:", slope1)
+    print("Intercept:", intercept1)
+    # print("Slope:", slope2)
+    # print("Intercept:", intercept2)
+    plt.plot(x1, x1 * slope1 + intercept1)
+    plt.plot(x1, y1, "o")
+    # plt.plot(np.vstack([x1,x1]), np.vstack([x1 * slope1 + intercept1, y1]))
     
+    # plt.plot(x2, x2 * slope2 + intercept2)
+    # plt.plot(x2, y2, "o")
+    # plt.plot(np.vstack([x2,x2]), np.vstack([x2 * slope2 + intercept2, y2]))
+    plt.show()
+
 if __name__ == "__main__":
     main()
--- hy-data-analysis-with-python-summer-2019/part05-e11_mystery_data/src/mystery_data.py
+++ hy-data-analysis-with-python-summer-2019/part05-e11_mystery_data/src/mystery_data.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
+import os
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 def mystery_data():
-    return []
+    f = os.path.dirname(os.path.realpath(__file__)) + "/mystery_data.tsv"
+    df = pd.read_csv(f, sep = "\t")
+    features =  df.iloc[:, :5]
+    response = df.iloc[:, -1]
+    fm = LinearRegression(fit_intercept=False)
+    fm.fit(features, response)
+    return fm.coef_
 
 def main():
     coefficients = mystery_data()
     # print the coefficients here
-    
+    for i in range(1, 6):
+        print(f"Coefficient of X{i} is {coefficients[i-1]}")
+
 if __name__ == "__main__":
     main()
