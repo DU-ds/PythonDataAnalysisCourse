c96f0cc739f307972ad02fd7c0c1904418c5fc1b
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-10-20 16:08:06 -0400

    working on ex 6.

--- hy-data-analysis-with-python-summer-2019/part06-e05_plant_clustering/src/plant_clustering.py
+++ hy-data-analysis-with-python-summer-2019/part06-e05_plant_clustering/src/plant_clustering.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import scipy
-
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
 
 def find_permutation(n_clusters, real_labels, labels):
     permutation=[]
@@ -13,7 +15,13 @@ def find_permutation(n_clusters, real_labels, labels):
     return permutation
 
 def plant_clustering():
-    return 0.0
+    df = load_iris()
+    fm = KMeans(3)
+    fm.fit(df.data)
+    permutation = find_permutation(3, df.target, fm.labels_)
+    new_labels = [ permutation[label] for label in fm.labels_]   # permute the labels
+    score = accuracy_score(df.target, new_labels)
+    return score
 
 def main():
     print(plant_clustering())
--- hy-data-analysis-with-python-summer-2019/part06-e06_nonconvex_clusters/src/nonconvex_clusters.py
+++ hy-data-analysis-with-python-summer-2019/part06-e06_nonconvex_clusters/src/nonconvex_clusters.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python3
 
+import os
 import pandas as pd
 import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import accuracy_score
 
+def find_permutation(n_clusters, real_labels, labels):
+    permutation=[]
+    n_outliers = 0
+    for i in range(n_clusters):
+        idx = labels == i
+        # Choose the most common label among data points in the cluster
+        new_label=scipy.stats.mode(real_labels[idx])[0][0]
+        if new_label == -1:
+            new_label = None
+            n_outliers += 1
+        else:
+            permutation.append(new_label)        
+    return (permutation, n_outliers)
 
 def nonconvex_clusters():
+    f = os.path.dirname(os.path.realpath(__file__))
+    df = pd.read_csv(f + "/data.tsv", sep = "\t")
+    lst = []
+    for e in np.arange(0.05, 0.2, 0.05):
+fm = DBSCAN(eps = e)
+fm.fit(df.loc[:,["X1", "X2"]])
+n_clusters = len(np.unique(fm.labels_))
+new_labels, n_outliers = find_permutation(n_clusters, df.loc[:,"y"], fm.labels_)
+new_labels = [new_labels[label] for label in fm.labels_]
+score = accuracy_score(df.loc[:,"y"], new_labels)
+lst.append(())
+        
     return pd.DataFrame()
 
 def main():
