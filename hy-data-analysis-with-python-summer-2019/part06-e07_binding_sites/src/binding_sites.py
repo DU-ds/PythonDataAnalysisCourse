525ed386306d1f41ed9129fa3589755cd5b75722
DU-ds <kenneth.collins@alumni.stonybrook.edu>
2019-10-23 20:27:13 -0400

    stll eworking on ex7

--- hy-data-analysis-with-python-summer-2019/part06-e07_binding_sites/src/binding_sites.py
+++ hy-data-analysis-with-python-summer-2019/part06-e07_binding_sites/src/binding_sites.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import pairwise_distances
+from scipy.stats import mode as spmode 
 
 from matplotlib import pyplot as plt
 
@@ -20,10 +21,19 @@ def toint(x):
     x = x.replace("G", 2)
     x = x.replace("T", 3)
     return x
+    
+def find_permutation(n_clusters, real_labels, labels):
+    permutation=[]
+    for i in range(n_clusters):
+        idx = labels == i
+        # Choose the most common label among data points in the cluster
+        new_label=spmode(real_labels[idx])[0][0]
+        permutation.append(new_label)
+    return permutation
 
 def get_features_and_labels(filename):
     #read data in
-    f = os.path.dirname(os.path.realpath(__file__))
+    f = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/"
     df = pd.read_csv(f + filename, sep = "\t")
     
     # transform the string X column to a feature matrix
@@ -48,8 +58,12 @@ def cluster_euclidean(filename):
     features, labels = get_features_and_labels(filename)
     #fit model and predict values
     y_hat = AgglomerativeClustering(n_clusters = 2, affinity = "euclidean", linkage = "average").fit_predict(features)
+    # fix labels
+    permutation = find_permutation(2, labels, y_hat)
+    new_labels = [ permutation[label] for label in y_hat]
+    y_hat_relable = pd.Series(new_labels)
     # return accuracy score
-    return accuracy_score(labels, y_hat)
+    return accuracy_score(labels, y_hat_relable)
 
 def cluster_hamming(filename):
     # get data ready
@@ -59,10 +73,14 @@ def cluster_hamming(filename):
     dist = pairwise_distances(features, metric = "hamming")
     ## fit model
     y_hat = AgglomerativeClustering(n_clusters = 2, affinity = "precomputed", linkage = "average").fit_predict(dist)
+    # fix labels
+    permutation = find_permutation(2, labels, y_hat)
+    new_labels = [ permutation[label] for label in y_hat]
+    y_hat_relable = pd.Series(new_labels)
     # plot distances
     plot(dist) #shows four square clusters, which seems to make sense -- dna has 4 nucleotides
     # return accuracy score
-    return accuracy_score(labels, y_hat)
+    return accuracy_score(labels, y_hat_relable)
 
  ###TODO fix labels for distance functions using the find_permutation function
  # Note that you may have to use the find_permutation function again, 
@@ -70,9 +88,17 @@ def cluster_hamming(filename):
  # differently than the real labels given in data.seq.
 
 def main():
-    print("Accuracy score with Euclidean affinity is", cluster_euclidean("/data.seq"))
-    print("Accuracy score with Hamming affinity is", cluster_hamming("/data.seq"))
+    print("Accuracy score with Euclidean affinity is", cluster_euclidean("src/data.seq"))
+    print("Accuracy score with Hamming affinity is", cluster_hamming("src/data.seq"))
 
 
 if __name__ == "__main__":
     main()
+"""
+Failed: test.test_binding_sites.BindingSites.test_features_and_labels
+        'DataFrame' object has no attribute 'flat'
+
+Failed: test.test_binding_sites.BindingSites.test_toint
+        replace() argument 2 must be str, not int
+
+"""
